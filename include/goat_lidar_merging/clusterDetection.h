1edff3be93b1712a7a954e9b62ab81140d1d167b
Griffin Tabor <gftabor@wpi.edu>
2016-11-02 20:54:38 -0400

    Now has global object list is uses to remove points from new scans
    Makes cluster computation not linearly proportional to time

--- include/goat_lidar_merging/clusterDetection.h
+++ include/goat_lidar_merging/clusterDetection.h
@@ -1,19 +1,37 @@
 #ifndef cluster_h
 #define cluster_h
 
+#include <algorithm>
+
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/common/common_headers.h>
+
+
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/console/time.h>
+
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
+#include <pcl/common/geometry.h>
+
 
 class clusterDetection
 {
   public:
     clusterDetection(unsigned int minPoints,unsigned int radiusMM);
     void cluster(sensor_msgs::PointCloud2 &cloud2);
+    static bool alreadyKnown(const pcl::PointXYZ &point);
+    void removeRedundantPoints(pcl::PointCloud<pcl::PointXYZ> &cloud);
+    static pcl::PointCloud<pcl::PointXYZ> objects; //temp clouds
+    static double squaredDistance;
 
     
   private:
-  	pcl::PointCloud<pcl::PointXYZ> nonClumpedPoints;
+	pcl::ConditionalEuclideanClustering<pcl::PointXYZ> * cec;
+  pcl::PointCloud<pcl::PointXYZ> nonClumpedPoints;
 
 };
 
--- src/clusterDetection.cpp
+++ src/clusterDetection.cpp
@@ -1,14 +1,35 @@
 #include "goat_lidar_merging/clusterDetection.h"
+double clusterDetection::squaredDistance;
+pcl::PointCloud<pcl::PointXYZ> clusterDetection::objects;
+
 
 clusterDetection::clusterDetection(unsigned int minPoints,unsigned int radiusMM){
+	cec = new pcl::ConditionalEuclideanClustering<pcl::PointXYZ> (true);
 
-
-
+	squaredDistance = (4.0*radiusMM * radiusMM)*1e-6;
+  	//cec->setConditionFunction (&customRegionGrowing);
+	cec->setClusterTolerance (radiusMM);
+	cec->setMinClusterSize (minPoints);
 }
+bool clusterDetection::alreadyKnown(const pcl::PointXYZ &point){
+	for(unsigned i=0; i <objects.size();i++){
+		if(((objects.at(i).x-point.x)*(objects.at(i).x-point.x) + (objects.at(i).y-point.y)*(objects.at(i).y-point.y))<squaredDistance)
+			return true;
+	}
+	return false;
+}
+void clusterDetection::removeRedundantPoints(pcl::PointCloud<pcl::PointXYZ> &cloud){
+	std::remove_if(cloud.begin(),cloud.end(),clusterDetection::alreadyKnown);
+	}
 
-void clusterDetection::cluster(sensor_msgs::PointCloud2 &cloud2){
+
+	void clusterDetection::cluster(sensor_msgs::PointCloud2 &cloud2){
     pcl::PointCloud<pcl::PointXYZ> newScan; //temp clouds
-	fromROSMsg(cloud2,newScan);
-	nonClumpedPoints+=newScan;
+    fromROSMsg(cloud2,newScan);
+    nonClumpedPoints+=newScan;
 
+	//cec->setInputCloud (nonClumpedPoints);
+
+	//cec->segment (*clusters);
+  	//cec->getRemovedClusters (small_clusters, large_clusters);
 }
